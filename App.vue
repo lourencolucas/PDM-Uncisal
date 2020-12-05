f939b5b668b971d42af867d8501390c183b6d2d5
lucaslourenco189 <49119918+lucaslourenco189@users.noreply.github.com>
2020-12-04 23:42:08 -0300

    Arquivos de imagens dos proptotipos de tela para a atividade

---
+++ App.vue
@@ -0,0 +1,47 @@
+<template>
+  <view class="container">
+    <status-bar background-color="black" bar-style="light-content" />
+    <text class="text-login">Login</text>
+    <text-input class="input" />
+    <text-input class="input" />
+    <text class="avisos2">Esqueceu a senha?</text>
+    <button class="btn-entrar" title="Login" color="black" :on-press="pressionar">
+  </view>
+</template>
+
+<script>
+import { Button } from 'react-native';
+export default {
+  methods: {
+    pressionar: function () {
+      alert("Olá, usuário!");
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  display: flex;
+}
+
+
+.avisos{
+  padding: 10px;
+  align-items: flex-end;
+}
+.text-login {
+  font-size: 40px;
+  color: black;
+}
+
+.input {
+  width: 90%;
+  margin: 10px 10px;
+  border-radius: 1px;
+  padding: 10px;
+  border-width: 1;
+}
+</style>
